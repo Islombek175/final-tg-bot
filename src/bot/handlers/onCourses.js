bfaf1708bf254dec940750b9fc5684d7ff624e76
Islombek Matkarimov <matkaimovislambek@gmail.com>
2025-12-19 18:11:25 +0500

    onCourses added all courses

--- src/bot/handlers/onCourses.js
+++ src/bot/handlers/onCourses.js
@@ -3,7 +3,22 @@ import bot from '../bot.js'
 const onCourses = chatId => {
 	bot.sendMessage(
 		chatId,
-		'🎓 Bizning o‘quv markazimizda quyidagi kurslar mavjud:\n\n1️⃣ Ingliz tili\n2️⃣ Rus tili\n3️⃣ Matematika\n4️⃣ Dasturlash (HTML, CSS va JavaScript)\n5️⃣ Grafik dizayn\n\n👇 Quyidagi kurslardan birini tanlang va batafsil ma’lumot oling:'
+		'🎓 Bizning o‘quv markazimizda quyidagi kurslar mavjud:\n\n1️⃣ Ingliz tili\n2️⃣ Rus tili\n3️⃣ Matematika\n4️⃣ Dasturlash (HTML, CSS va JavaScript)\n5️⃣ Grafik dizayn\n\n👇 Quyidagi kurslardan birini tanlang va batafsil ma’lumot oling:',
+		{
+			reply_markup: {
+				inline_keyboard: [
+					[
+						{ text: '🇬🇧 Ingliz tili', callback_data: 'course_english' },
+						{ text: '🇷🇺 Rus tili', callback_data: 'course_russian' },
+					],
+					[
+						{ text: '🧮 Matematika', callback_data: 'course_math' },
+						{ text: '💻 Dasturlash', callback_data: 'course_programming' },
+					],
+					[{ text: '🎨 Grafik dizayn', callback_data: 'course_design' }],
+				],
+			},
+		}
 	)
 }
 
